e4681dd0cfccb8db7973e4c4c557aab3511d8a42
Juanjo <olmedix@gmail.com>
2024-11-12 23:29:38 +0100

    primer commit

---
+++ database/migrations/0001_01_00_184020_create_spaces_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spaces', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('regNumber', 10)->unique();
+            $table->string('observation_CA', 5000)->nullable();
+            $table->string('observation_ES', 5000)->nullable();
+            $table->string('observation_EN', 5000)->nullable();
+            $table->string('email',100);
+            $table->string('phone',100);
+            $table->string('website',100);
+            $table->enum('access_types',['y','n','p']);
+            $table->unsignedInteger('total_scores');
+            $table->unsignedInteger('count_scores');
+            $table->foreignId('address_id')->constrained();
+            $table->foreignId('space_types_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spaces');
+    }
+};
---
+++ database/migrations/0001_01_00_184022_create_space_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('space_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment',100);
+            $table->unsignedInteger('score');
+            $table->enum('status',['y','n']);
+            $table->foreignId('space_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('space_user');
+    }
+};

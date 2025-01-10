4349ac2b5d4ba27c38dd24fbf0ebc7a65ffa100b
olmedix <olmedix@gmail.com>
2025-01-10 12:32:09 +0100

    ruta update sin middleware para poder restablecer password

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,8 @@ use App\Http\Controllers\Api\AuthController;
 //AUTH
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
+//CAMBIO PARA PERDIDA DE CONTRASEÑA
+Route::put('/user/{value}', [UserController::class, 'update']);
 
 
 Route::middleware([ApiKeyMiddleware::class])->group(function () {
@@ -23,7 +24,7 @@ Route::middleware([ApiKeyMiddleware::class])->group(function () {
     });
 
     // Rutas protegidas por API Key Middleware
-    Route::put('/user/{value}', [UserController::class, 'update']);
+    
     Route::apiresource('user', UserController::class);
     Route::apiresource('spaces', SpaceController::class);
     Route::get('/municipalities', [MunicipalityController::class, 'index']);
